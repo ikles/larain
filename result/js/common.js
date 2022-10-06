229c1b81dd5d26f06b66997fce5ed4dec5214465
Igor <membship@ya.ru>
2022-10-06 23:25:39 +0700

    first commit

---
+++ result/js/common.js
@@ -0,0 +1,86 @@
+jQuery(document).ready(function( $ ) {
+
+  $(".toggle-mnu").click(function() {
+    $(this).toggleClass("on");
+    $(".top-mnu").slideToggle();
+    return false;
+  });
+
+  $('body').click(function () {
+    if( $(".toggle-mnu").hasClass("on") ){
+      $(".toggle-mnu").removeClass("on");
+      $(".top-mnu").fadeOut();
+    }
+  });
+
+
+  $(".top-mnu").click(function (e) {
+    e.stopPropagation();
+  });
+
+
+
+
+/************************************/
+
+/*$('.wrapper').prepend('<span class="eye-3"></span>');
+let pg = parseInt(document.location.pathname.match(/\d+/))
+$('body').addClass('active').css('background-image', "url('../img/"+pg+".jpg')");
+$('body:not(.active)').css('background-image', "unset");
+
+$('.eye-3').click(function (e) {
+  e.preventDefault();  
+  $('body').toggleClass('active');
+  let pg = parseInt(document.location.pathname.match(/\d+/));
+  $('body.active').css('background-image', "url('../img/"+pg+".jpg')");
+  $('body:not(.active)').css('background-image', "unset");
+
+});*/
+
+/************************************/
+
+
+
+function popup(openLink, windowEl, closeEl) {  
+  $(openLink).click(function(e) {
+    e.preventDefault();
+    $(windowEl).fadeIn();
+    $('body').addClass('ohi');
+  });
+  $(closeEl).click(function(e) {
+    e.preventDefault();
+    $(windowEl).fadeOut();
+    $('body').removeClass('ohi');
+  });
+  $('.modal-overlay').click(function () {
+    $(this).fadeOut();
+    $('body').removeClass('ohi');
+  });
+  $('.modal-form__block').click(function (e) {
+    e.stopPropagation();  
+  });
+  
+}
+
+popup('.link2', '.modal-overlay_2', '.modal-close_2');
+popup('.link', '.modal-overlay_1', '.modal-close_1');
+
+
+$('a[href*=\\#]:not([href=\\#])').click(function () {
+  elementClick = $(this).attr("href");
+  destination = $(elementClick).offset().top;
+  $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination - 85}, 1100);
+  return false;
+});
+
+
+$('.calculate-form__col').click(function () {
+  $(this)
+  .addClass('_active')
+  .siblings().removeClass('_active');
+});
+
+$(".input-phone").mask("+7 (999) 999-99-99");
+
+}); //ready
+
